4ecb0af9ea2aa1ac1614e6b07c273a7f715b2738
RobertOchmanek <robx34@gmail.com>
2022-04-27 19:51:48 +0200

    Move validation and processing

--- src/processor/state/GameState.java
+++ src/processor/state/GameState.java
@@ -3,6 +3,8 @@ package processor.state;
 import interfaces.KalahaState.GameStates;
 import processor.TurnProcessor;
 
+import java.util.Map;
+
 public abstract class GameState {
 
     protected final TurnProcessor turnContext;
@@ -14,4 +16,8 @@ public abstract class GameState {
     public abstract void processTurn();
 
     public abstract GameStates getStateType();
+
+    public abstract boolean validateMove(int house, int houses, Map<Integer, Integer> board);
+
+    public abstract void makeMove(int house, Map<Integer, Integer> board);
 }
