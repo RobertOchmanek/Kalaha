e8007d3316c9f72217d06f303b050a28f69f38df
RobertOchmanek <robx34@gmail.com>
2022-04-27 19:10:51 +0200

    Initialize board

--- src/processor/TurnProcessor.java
+++ src/processor/TurnProcessor.java
@@ -5,14 +5,19 @@ import players.PlayersManager;
 import processor.state.GameState;
 import processor.state.InitialState;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TurnProcessor {
 
+    private final Map<Integer, Integer> board;
     private GameState currentState;
     private ObserversManager observersManager;
     private PlayersManager playersManager;
 
     public TurnProcessor() {
         this.currentState = new InitialState(this);
+        this.board = new HashMap<>();
     }
 
     public void changeState(GameState nextState) {
@@ -24,7 +29,15 @@ public class TurnProcessor {
     }
 
     public void initializeBoard(int houses, int seeds) {
-
+        int totalFields = (2 * houses) + 2;
+        for (int field = 0; field < totalFields; ++field) {
+            if (field == houses || field == (2 * houses) + 1) {
+                board.put(field, 0);
+            } else {
+                board.put(field, seeds);
+            }
+        }
+        System.out.println(board);
     }
 
     public void setObserversManager(ObserversManager observersManager) {
