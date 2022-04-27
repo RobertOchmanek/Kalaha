89fc11fcafd7d7691d9dc3d43f713b3770574d22
daniel2440 <daniel.koszyk024@gmail.com>
2022-04-27 19:01:44 +0200

    + ObserversManager.java and its usage

---
+++ src/observers/GameObserversManager.java
@@ -0,0 +1,27 @@
+package observers;
+
+import interfaces.GameStateObserver;
+import interfaces.KalahaState;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameObserversManager implements ObserversManager {
+    private final List<GameStateObserver> gameObservers;
+
+    public GameObserversManager() {
+        this.gameObservers = new ArrayList<>();
+    }
+
+    @Override
+    public void notifyObservers(KalahaState kalahaState) {
+        for (GameStateObserver observer : gameObservers) {
+            observer.currentState(kalahaState);
+        }
+    }
+
+    @Override
+    public void addObserver(GameStateObserver observer) {
+        gameObservers.add(observer);
+    }
+}
