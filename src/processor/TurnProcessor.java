025985f26472a3dd914d941ebc434118b4b08716
RobertOchmanek <robx34@gmail.com>
2022-04-27 18:41:26 +0200

    Add observers interfaces

--- src/processor/TurnProcessor.java
+++ src/processor/TurnProcessor.java
@@ -1,11 +1,15 @@
 package processor;
 
+import observers.ObserversManager;
+import players.PlayersManager;
 import processor.state.GameState;
 import processor.state.InitialState;
 
 public class TurnProcessor {
 
     private GameState currentState;
+    private ObserversManager observersManager;
+    private PlayersManager playersManager;
 
     public TurnProcessor() {
         this.currentState = new InitialState(this);
@@ -17,5 +21,17 @@ public class TurnProcessor {
 
     public void processTurn() {
         currentState.processTurn();
+    }
+
+    public void initializeBoard(int houses, int seeds) {
+
+    }
+
+    public void setObserversManager(ObserversManager observersManager) {
+        this.observersManager = observersManager;
+    }
+
+    public void setPlayersManager(PlayersManager playersManager) {
+        this.playersManager = playersManager;
     }
 }
